253117ebf6a3fe6251c3f7b83ef356c3d18899ee
Sherylleong <sherylleongkitye@gmail.com>
2024-04-06 16:47:18 +0800

    added some form fields

---
+++ frontend/Components/Form/Form.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import { Input } from '@chakra-ui/react'
+
+export const Form = () => {
+    return (
+        <>
+            <Input placeholder='Enter Number of Employees'/>
+            <Input placeholder='Enter CO2 Emissions'/>
+        </>
+    );
+};
